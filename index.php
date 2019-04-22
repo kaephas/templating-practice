c6fddf3d5b8051ef65a920e1b224a4183a906763
Kaephas <kkain@mail.greenriver.edu>
2019-04-22 16:07:02 -0400

    add initial setup files

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaephas Kain
+ * Date: 2019-04-12
+ * Filename: index.php
+ * Description: loads error reporting, composer, fat free, setting default route to views/home.html
+ */
+
+session_start();
+
+//Turn on error reporting
+ini_set('display_errors' ,1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base:: instance();
+
+//Turn on Fat-free error reporting
+$f3 -> set('DEBUG', 3);
+
+//Define a default route (dating splash page)
+$f3->route('GET /', FUNCTION()
+{
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//run Fat-free
+$f3 -> run();
